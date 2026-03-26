56309cea08560dbc400ee92ff1f0e1c20c30b772
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-03-26 19:05:24 +0900

    fix: formatted files

--- fabric/src/main/java/com/karasu256/projectk/fabric/ProjectKFabric.java
+++ fabric/src/main/java/com/karasu256/projectk/fabric/ProjectKFabric.java
@@ -1,9 +1,9 @@
 package com.karasu256.projectk.fabric;
 
 import com.karasu256.projectk.ProjectK;
+import com.karasu256.projectk.api.integration.ModIntegrationBootstrapper;
 import com.karasu256.projectk.fabric.config.ProjectKFabricConfig;
 import com.karasu256.projectk.fabric.integrations.FabricModIntegrationSupplier;
-import com.karasu256.projectk.api.integration.ModIntegrationBootstrapper;
 import net.fabricmc.api.ModInitializer;
 
 public final class ProjectKFabric implements ModInitializer {
