88e987b4b673a1dcda10a3f55ad0b566784d8d0e
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-04-15 14:02:37 +0900

    fix: fixed percentTo logic

--- common/src/main/java/com/karasu256/projectk/utils/MathUtils.java
+++ common/src/main/java/com/karasu256/projectk/utils/MathUtils.java
@@ -3,6 +3,6 @@ package com.karasu256.projectk.utils;
 public final class MathUtils {
     public static float percentTo(float percent, float max) {
         float clamped = Math.max(0.0f, Math.min(1.0f, percent));
-        return Math.round(clamped * 16.0f);
+        return Math.round(clamped * max);
     }
 }
